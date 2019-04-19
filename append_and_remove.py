f769f3d3e71e1d1d138431be72c604d2662a3630
igor popenov <ipopenov@protonmail.com>
2019-04-19 16:51:18 -0400

    Day 4 initial commit

---
+++ append_and_remove.py
@@ -0,0 +1,12 @@
+power_rangers = ["Jason","Trini","Billy","Zack","Kim"]
+
+# add Tommy
+
+power_rangers.append("Tommy")
+
+print(power_rangers)
+
+# remove Tommy
+power_rangers.remove("Tommy")
+
+print(power_rangers)
